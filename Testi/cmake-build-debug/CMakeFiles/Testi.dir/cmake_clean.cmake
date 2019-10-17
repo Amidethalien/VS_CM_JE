839dad9d4adcbe1000140a3ec9ef22e22978195b
Amidethalien <christoph.meyer@stud.h-da.de>
2019-10-17 10:37:31 +0200

    first commit

---
+++ Testi/cmake-build-debug/CMakeFiles/Testi.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Testi.dir/main.cpp.o"
+  "Testi.pdb"
+  "Testi"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Testi.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
