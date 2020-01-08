efa16b10f110228c5f1ec43c4fec3a19dbb9c668
Amidethalien <christoph.meyer@stud.h-da.de>
2020-01-08 16:45:35 +0100

    Erster Auto-Sensor: Generierung der Daten fertig, Socket in Arbeit

--- Auto_Socket_1/main.cpp
+++ Auto_Socket_1/main.cpp
@@ -1,4 +1,98 @@
 #include <iostream>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+
+struct Position{
+    int x;
+    int y;
+};
+
+void generate_Direction(int &direction){
+    direction = rand() % 4;
+    return;
+}
+
+void generate_Speed( int &speed){
+    if(rand()%130>speed){
+        speed=speed+(rand()%20+1);
+    }
+    else{
+        speed=speed-(rand()%20+1);
+    }
+    return;
+}
+
+void generate_Position(const int &direction, const int &speed, Position &pos){
+    if(direction==0){
+        if(speed==0){
+            return;;
+        }
+        else if(speed<=40){
+            pos.y=(pos.y+1)%50;
+            return;
+        }
+        else if(speed<=80){
+            pos.y=(pos.y+2)%50;
+            return;
+        }
+        else if(speed<=130){
+            pos.y=(pos.y+3)%50;
+            return;
+        }
+    }
+    else if(direction==1){
+        if(speed==0){
+            return;;
+        }
+        else if(speed<=40){
+            pos.x=(pos.x+1)%50;
+            return;
+        }
+        else if(speed<=80){
+            pos.x=(pos.x+2)%50;
+            return;
+        }
+        else if(speed<=130){
+            pos.x=(pos.x+3)%50;
+            return;
+        }
+    }
+    else if(direction==2){
+        if(speed==0){
+            return;;
+        }
+        else if(speed<=40){
+            pos.y=(pos.y-1)%50;
+            return;
+        }
+        else if(speed<=80){
+            pos.y=(pos.y-2)%50;
+            return;
+        }
+        else if(speed<=130){
+            pos.y=(pos.y-3)%50;
+            return;
+        }
+    }
+    else if(direction==3){
+        if(speed==0){
+            return;
+        }
+        else if(speed<=40){
+            pos.x=(pos.x-1)%50;
+            return;
+        }
+        else if(speed<=80){
+            pos.x=(pos.x-2)%50;
+            return;
+        }
+        else if(speed<=130){
+            pos.x=(pos.x-3)%50;
+            return;
+        }
+    }
+}
 
 
 
@@ -6,22 +100,40 @@ int main() {
 
     int my_socket = -1;
     int result = -1;
+    char buff[256];
+    int portno=-1;
 
-    char Fahrtrichtung='-1';
-    int Geschwindigkeit=-1;
-    struct Position{
-        int x;
-        int y;
-    };
+    my_socket= socket(AF_INET, SOCK_DGRAM, 0);
+    if(my_socket<0){
+        perror("failed to create UDP socket\n");
+        return -1;
+    }
+    std::cout<<"Portnummer:";
+    scanf("%d", &portno);
+    std::cout<<"Eingabe bestätigt\n";
+
+    sockaddr_in server_addr;
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(portno);
+    server_addr.sin_addr=;
+
+
+
+
+    int Direction=-1; //0=Norden 1=Osten 2=Süden 3=Westen
+    int Speed=rand()%130;
+
     int PosX=-1;
     int PosY=-1;
 
     Position pos;
-    pos.x=0;
-    pos.y=0;
+    pos.x=rand()%50;
+    pos.y=rand()%50;
 
     while(1){
-
+    generate_Direction(Direction);
+    generate_Speed(Speed);
+    generate_Position(Direction,Speed,pos);
     }
 
     return 0;
